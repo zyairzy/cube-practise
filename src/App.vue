fc0e7d102119fa1af292aefe956ee0e9afa63a5e
zyairzy <zy2uestc@gmail.com>
2019-11-22 16:12:51 +0800

    update MatchList ui with default data

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,12 @@
       <div class="title">全部赛事</div>
       <div class="navigator">
         <ul class="nav-list">
-          <li v-for="(item, index) in tabList" :key="index"
-            @click="switchTab(index)" :class="{active: currentPage === index}">
-            {{ item }}
-          </li>
+          <li
+            v-for="(item, index) in tabList"
+            :key="index"
+            @click="switchTab(index)"
+            :class="{active: currentPage === index}"
+          >{{ item }}</li>
         </ul>
         <div class="triangle-up" :class="{left: currentPage === 0, right: currentPage === 2}"></div>
       </div>
@@ -19,10 +21,11 @@
         :loop="false"
         :autoPlay="false"
         :threshold="0.1"
-        @change="slideChange">
+        @change="slideChange"
+      >
         <cube-slide-item v-for="(item, index) in tabList" :key="index">
           <div class="match-list-wrapper">
-            <match-list :type="index"></match-list>
+            <match-list :type="type" :status="index"></match-list>
           </div>
         </cube-slide-item>
         <div slot="dots"></div>
@@ -38,7 +41,8 @@ export default {
   data () {
     return {
       currentPage: 1,
-      tabList: ['已结束', '直播中', '我的关注']
+      tabList: ['已结束', '直播中', '我的关注'],
+      type: 'soccer'
     }
   },
   components: {
@@ -56,49 +60,73 @@ export default {
 </script>
 
 <style lang="stylus">
-html, body, #app
-  height: 100%
-  text-align: center
-#app
-  background-color: #E0E4E8
-  .header
-    color: white
-    background-color: #15191D
-    .title
-      padding: 20px 0
-      font-size: 16px
-      color: white
-    .navigator
-      position: relative
-      padding-bottom: 12px
-      font-size: 12px
-      .nav-list
-        display: flex
-        justify-content: space-around
-        li
-          width: 60px
-          color: #636873
-          &.active
-            color: white
-      .triangle-up
-        position: absolute
-        left: 50%
-        transform: translate(-50%, 0)
-        bottom: 0
-        width: 0
-        height: 0
-        border-left: 7px solid transparent
-        border-right: 7px solid transparent
-        border-bottom: 8px solid #E0E4E8
-        transition: all 0.4s
-        &.left
-          left: 16.67%
-        &.right
-          left: 83.34%
-  .content
-    height: calc(100% - 80px)
-    overflow: hidden
-    .match-list-wrapper
-      height: 100%
-  
+html, body, #app {
+  height: 100%;
+  text-align: center;
+}
+
+#app {
+  background-color: #E0E4E8;
+
+  .header {
+    color: white;
+    background-color: #15191D;
+
+    .title {
+      padding: 20px 0;
+      font-size: 16px;
+      color: white;
+    }
+
+    .navigator {
+      position: relative;
+      padding-bottom: 12px;
+      font-size: 12px;
+
+      .nav-list {
+        display: flex;
+        justify-content: space-around;
+
+        li {
+          width: 60px;
+          color: #636873;
+
+          &.active {
+            color: white;
+          }
+        }
+      }
+
+      .triangle-up {
+        position: absolute;
+        left: 50%;
+        transform: translate(-50%, 0);
+        bottom: 0;
+        width: 0;
+        height: 0;
+        border-left: 7px solid transparent;
+        border-right: 7px solid transparent;
+        border-bottom: 8px solid #E0E4E8;
+        transition: all 0.4s;
+
+        &.left {
+          left: 16.67%;
+        }
+
+        &.right {
+          left: 83.34%;
+        }
+      }
+    }
+  }
+
+  .content {
+    height: calc(100% - 80px);
+    overflow: hidden;
+
+    .match-list-wrapper {
+      height: 100%;
+    }
+  }
+}
 </style>
--- src/components/MatchList.vue
+++ src/components/MatchList.vue
@@ -8,19 +8,37 @@
       @pulling-up="onPullingUp"
     >
       <ul>
-        <li v-for="(v,i) in matchList" :key="i" style="height:30px">{{v}}</li>
+        <li v-for="(item,i) in matchList" :key="i" class="match-item">
+          <div class="left-side">
+            <img :src="item.hostLogoUrl" alt class="logo" />
+            <p class="name">{{item.hostTeamName}}</p>
+          </div>
+          <div class="center-side">
+            <p v-if="item.live" class="live" :class="{end : item.isEnd}">{{item.live}}</p>
+            <p v-if="item.order" class="order">{{item.order}}</p>
+            <p class="score">{{item.hostScore}} - {{item.guestScore}}</p>
+            <p class="time">{{item.isEnd? "" : item.endTime}}</p>
+          </div>
+          <div class="right-side">
+            <img :src="item.guestLogoUrl" alt class="logo" />
+            <p class="name">{{item.guestTeamName}}</p>
+          </div>
+        </li>
       </ul>
     </cube-scroll>
   </div>
 </template>
 
 <script>
+
+import matchData from '../common/data/match-list'
+
 export default {
   name: 'match-list',
   props: ['type', 'status'],
   data () {
     return {
-      matchList: ['比赛1', '比赛2', '比赛1', '比赛2', '比赛1', '比赛2', '比赛1', '比赛2', '比赛1', '比赛2', '比赛1', '比赛2', '比赛1', '比赛2', '比赛1', '比赛2', '比赛1', '比赛2', '比赛1', '比赛2', '比赛1', '比赛3'],
+      matchList: matchData[this.type][this.status],
       options: {
         scrollbar: {
           fade: true
@@ -46,7 +64,7 @@ export default {
         if (Math.random() > 0.5) {
           let match = []
           for (let index = 5; index > 0; index--) {
-            match.push('this.matchList[index]')
+            match.push(this.matchList[index])
           }
           this.matchList.unshift(...match)
         } else {
@@ -59,7 +77,7 @@ export default {
         if (Math.random() > 0.5) {
           let match = []
           for (let index = 5; index < 10; index++) {
-            match.push('bbb')
+            match.push(this.matchList[index])
           }
           this.matchList = this.matchList.concat(match)
         } else {
@@ -74,6 +92,67 @@ export default {
 <style lang="stylus">
 .match-list {
   height: 618px;
-  background-color: #E2E5EA;
+  background-color: #FFFFFF;
+
+  .match-item {
+    border-bottom: 1px solid #E4E4E4;
+    padding: 10px 0;
+    display: flex;
+    justify-content: space-around;
+
+    .left-side, .right-side {
+      width: 80px;
+
+      .logo {
+        display: inline-block;
+        width: 38px;
+        height: 38px;
+        margin-bottom: 7px;
+      }
+
+      .name {
+        font-size: 14px;
+      }
+    }
+
+    .center-side {
+      width: 80px;
+      font-size: 12px;
+
+      .live {
+        background-color: #3D8F29;
+        color: white;
+        line-height: 16px;
+        padding: 3px 10px;
+        border-radius: 25px;
+        margin-bottom: 7px;
+      }
+
+      .end {
+        background-color: grey;
+      }
+
+      .order {
+        border: 1px #2F6220 solid;
+        line-height: 16px;
+        padding: 3px 10px;
+        border-radius: 25px;
+        margin-bottom: 7px;
+      }
+
+      .score {
+        font-size: 14px;
+        font-weight: 500;
+        margin-bottom: 7px;
+        position: relative;
+      }
+
+      .time {
+        color: grey;
+        line-height: 16px;
+        text-align: center;
+      }
+    }
+  }
 }
 </style>
